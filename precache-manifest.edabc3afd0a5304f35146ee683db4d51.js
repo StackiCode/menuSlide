accf1990b9a211874067765e821d8698a3fa1679
StackiCode <stacki.code@gmail.com>
2020-07-06 09:36:48 +0700

    Updates

---
+++ precache-manifest.edabc3afd0a5304f35146ee683db4d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328541db9fc521e83928007618718366",
+    "url": "/menuSlide/index.html"
+  },
+  {
+    "revision": "ad0bfb6ade5895a71e08",
+    "url": "/menuSlide/static/css/main.79d73e1f.chunk.css"
+  },
+  {
+    "revision": "043984bb5b6f1dcc62f3",
+    "url": "/menuSlide/static/js/2.584688ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menuSlide/static/js/2.584688ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0bfb6ade5895a71e08",
+    "url": "/menuSlide/static/js/main.91c85992.chunk.js"
+  },
+  {
+    "revision": "0c53bbb54499af607268",
+    "url": "/menuSlide/static/js/runtime-main.38fd2539.js"
+  }
+]);
